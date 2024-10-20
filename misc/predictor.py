d05ded66bb2f7c3c451b57ebe0a07a8aa4d50bc8
kesvill <168113229+AdilHAA@users.noreply.github.com>
2024-10-20 12:42:52 +0300

    upd

---
+++ misc/predictor.py
@@ -0,0 +1,20 @@
+from catboost import CatBoostClassifier
+import joblib
+from sklearn.preprocessing import LabelEncoder
+import numpy as np 
+import pandas as pd
+import os
+
+
+loaded_encoder = joblib.load(os.path.join(os.getcwd(), "models", "w20_10_6_35.pkl"))
+
+model = CatBoostClassifier()
+model.load_model(os.path.join(os.getcwd(), "models", "w20_10_6_12.cbm"))
+
+
+def get_prediction(inference_dataframe: pd.DataFrame) -> list:
+    y_pred = model.predict(inference_dataframe.drop('id',axis =1))
+    y_pred_decoded = loaded_encoder.inverse_transform(y=y_pred)
+    answer = pd.DataFrame({'id': inference_dataframe['id'], 
+                            'target': y_pred_decoded})
+    return answer["target"].tolist()
